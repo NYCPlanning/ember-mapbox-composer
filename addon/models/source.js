b7971a29eb26182a17485bffcf4f7166fa5f15e8
allthesignals <wilburnforce@gmail.com>
2018-05-18 10:43:31 -0400

    Migrate in-app-repo; add dependencies

---
+++ addon/models/source.js
@@ -0,0 +1,11 @@
+import DS from 'ember-data';
+import { attr } from 'ember-decorators/data';
+
+const { Model } = DS;
+
+export default Model.extend({
+  @attr('string') type: null,
+  @attr() 'source-layers': null,
+  @attr() tiles: null,
+  @attr('number') tileSize: null,
+});
