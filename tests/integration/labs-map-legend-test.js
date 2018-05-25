ec802624f8b4ca00b9750a3ea3b45e80b8ebe060
Chris Whong <chris@whong.org>
2018-05-25 15:37:23 -0400

    add conditional legend-item tooltip

--- tests/integration/labs-map-legend-test.js
+++ tests/integration/labs-map-legend-test.js
@@ -1,6 +1,6 @@
 import { module, test } from 'qunit';
-import { setupRenderingTest } from 'ember-qunit';
-import { render } from '@ember/test-helpers';
+import { setupRenderingTest, pauseTest } from 'ember-qunit';
+import { render, find } from '@ember/test-helpers';
 import hbs from 'htmlbars-inline-precompile';
 
 module('Integration | Component | labs-map-legend', function(hooks) {
@@ -22,5 +22,24 @@ module('Integration | Component | labs-map-legend', function(hooks) {
     `);
 
     assert.equal(this.element.textContent.trim(), 'template block text');
+  });
+
+  test('it shows no tooltip by default', async function(assert) {
+    // Set any properties with this.set('myProperty', 'value');
+    // Handle any actions with this.set('myAction', function(val) { ... });
+    this.set('model', {
+      items: [{
+        type: 'line',
+        style: {},
+        label: 'test-label',
+      }]
+    });
+
+    await render(hbs`{{labs-map-legend model=model}}`);
+
+    const tooltip = await find('.tooltip');
+    console.log('tooltip', tooltip)
+    assert.equal(!!tooltip, false);
+
   });
 });
