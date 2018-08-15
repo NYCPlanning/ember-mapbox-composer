284c272c57cef5889131c88e0c9fd0e02632fe82
allthesignals <wilburnforce@gmail.com>
2018-08-15 15:09:31 -0400

    Remove mirage config

--- tests/dummy/config/environment.js
+++ tests/dummy/config/environment.js
@@ -6,9 +6,6 @@ module.exports = function(environment) {
     environment,
     rootURL: '/',
     locationType: 'auto',
-    'ember-cli-mirage': {
-      enabled: false,
-    },
     EmberENV: {
       FEATURES: {
         // Here you can enable experimental features on an ember canary build
