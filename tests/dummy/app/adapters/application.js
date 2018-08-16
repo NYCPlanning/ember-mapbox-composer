d03a8449447386c60b6e94cb001adc8071a22342
allthesignals <wilburnforce@gmail.com>
2018-08-16 19:13:26 -0400

    Import as full module

--- tests/dummy/app/adapters/application.js
+++ tests/dummy/app/adapters/application.js
@@ -1,7 +1,10 @@
 import DS from 'ember-data';
-import { host } from '../config/environment';
+import config from '../config/environment';
 
+const { host } = config;
 const { JSONAPIAdapter } = DS;
+
+console.log(host);
 
 export default class ApplicationAdapter extends JSONAPIAdapter {
   host = host;
