1ad17bb6100f919447f49052a739a41c422bc97a
allthesignals <wilburnforce@gmail.com>
2018-10-11 15:38:52 -0400

    Add basic docs

--- addon/models/source.js
+++ addon/models/source.js
@@ -1,8 +1,14 @@
 import DS from 'ember-data';
-import { attr } from '@ember-decorators/data';
+import { attr, belongsTo } from '@ember-decorators/data';
 
 const { Model } = DS;
 
+/**
+  Model for related sources
+
+  @public
+  @class SourceModel
+*/
 export default class SourceModel extends Model {
   @attr() meta;
 }
