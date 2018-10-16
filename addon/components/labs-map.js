e866c7da6f2cf79e377a6a336a0576077eedac8c
allthesignals <wilburnforce@gmail.com>
2018-10-16 10:36:20 -0400

    Document simplified labs-map

--- addon/components/labs-map.js
+++ addon/components/labs-map.js
@@ -2,6 +2,29 @@ import mapboxGlMap from 'ember-mapbox-gl/components/mapbox-gl';
 import { argument } from '@ember-decorators/argument';
 import layout from '../templates/components/labs-map';
 
+
+/**
+  Extends `mapbox-gl` component to yield `labs-layers` contextual component. Allows passage of layer-groups.
+
+  ```js
+  // routes/application.js
+  import Route from '@ember/routing/route';
+
+  export default class ApplicationRoute extends Route {
+    async model() {
+      return this.store.query('layer-group');
+    }
+  }
+
+  ```
+  ```handlebars
+  {{!-- routes/application.hbs --}}
+  {{labs-map layerGroups=model}}
+  ```
+
+  @class LabsMapComponent
+  @public
+*/
 export default class MainMapComponent extends mapboxGlMap {
   constructor(...args) {
     super(...args);
@@ -17,6 +40,12 @@ export default class MainMapComponent extends mapboxGlMap {
 
   layout = layout;
 
+  /**
+    Collection of layer-group models (see: [DS.RecordArray](https://emberjs.com/api/ember-data/release/classes/DS.RecordArray)).
+    Allows optional `meta` object with a `mapboxStyle` property which is passed to the mapbox-gl instance.
+    @argument layerGroups
+    @type DS.RecordArray
+  */
   @argument
   layerGroups = null;
 }
